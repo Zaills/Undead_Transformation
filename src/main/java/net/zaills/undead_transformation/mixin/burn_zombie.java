8dcc1b3c87fdbffa606c81a11057b0db10fde0b3
Zaills <39807152+Zaills@users.noreply.github.com>
2025-06-30 14:50:14 +0200

    Zombie will not transform if hit by a player

--- src/main/java/net/zaills/undead_transformation/mixin/burn_zombie.java
+++ src/main/java/net/zaills/undead_transformation/mixin/burn_zombie.java
@@ -1,22 +1,37 @@
 package net.zaills.undead_transformation.mixin;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.ZombieEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ZombieEntity.class)
 public abstract class burn_zombie {
 
+	@Unique
+	private boolean attackedByPlayer = false;
+
 	@Shadow protected abstract void convertTo(EntityType<? extends ZombieEntity> entityType);
+
+	@Inject(at = @At("HEAD"), method = "damage")
+	private void onAttackedByPlayer(ServerWorld world, DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+		if (!attackedByPlayer && source.getAttacker() instanceof PlayerEntity) {
+			attackedByPlayer = true;
+		}
+	}
 
 	@Inject(at = @At("HEAD"), method = "tick()V")
 	private void tick(CallbackInfo info) {
 		ZombieEntity entity = (ZombieEntity) (Object) this;
-		if (entity.isOnFire() && entity.getHealth() <= 0) {
+		if (entity.isOnFire() && entity.getHealth() <= 1 && !attackedByPlayer) {
 			this.convertTo(EntityType.HUSK);
 			entity.setHealth(entity.getMaxHealth());
 		}
